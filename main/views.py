ea899f7334a5c4a8f5788b4874a04bbfca811946
Olivier-Denis847 <olivierphilippedenis@gmail.com>
2025-10-29 11:42:23 -0400

    Set up starter pages

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+class promptForm(forms.Form):
+    prompt = forms.CharField(label='Enter your prompt', max_length=15)
+
+def index(request):
+    if request.method == 'POST':
+        form = promptForm(request.POST)
+        if form.is_valid():
+            prompt = form.cleaned_data['prompt']
+            return HttpResponseRedirect(reverse('landing', args=[prompt]))
+    return render(request, 'main/index.html', 
+                  {'form': promptForm()})
+
+def landing(request, prompt):
+    return render(request, 'main/landing.html')
